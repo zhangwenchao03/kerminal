b8fe9dac0731b068fa660c9a01923d41c9e7b985
kongweiguang <240524885@qq.com>
2026-07-15 00:56:22 +0800

    refactor(command-palette): remove unused facade exports

--- src/features/command-palette/index.ts
+++ src/features/command-palette/index.ts
@@ -4,12 +4,5 @@ export {
 } from "./CommandPalette";
 export {
   buildCommandPaletteItems,
-  formatCommandPaletteEffect,
   scoreCommandPaletteAction,
 } from "./commandPaletteModel";
-export type {
-  CommandPaletteActionItem,
-  CommandPaletteActionPresentation,
-  CommandPaletteExecutionFeedback,
-  CommandPaletteProps,
-} from "./commandPaletteTypes";
